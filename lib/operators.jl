39afb5cc4ea690f6dfc38c063b5ee7b1caecf84e
Davide Ferracin <dv.ferracin@gmail.com>
2021-12-17 20:19:09 +0100

    Uniti op. Hamilt. e Lindblad in twositeoperators
    Una volta che la scelta dell'operatore locale/di interazione viene fatta
    dalla funzione `twositeoperators`, è inutile distinguere i casi in cui
    si usa l'evoluzione unitaria o quella alla Lindblad; si duplicherebbe
    inutilmente il codice.
    Oltretutto, la funzione corrispondente a `twositeoperators` per gli
    operatori di Lindblad non era nemmeno mai stata implementata… tanto vale
    mettere tutto assieme.

--- lib/operators.jl
+++ lib/operators.jl
@@ -28,78 +28,58 @@ function twositeoperators(sites::Vector{Index{Int64}},
   for j ∈ 1:length(sites)-1
     s1 = sites[j]
     s2 = sites[j+1]
-    h = 0.5localcfs[j] * Hlocal(s1) * op("Id", s2) +
-        0.5localcfs[j+1] * op("Id", s1) * Hlocal(s2) +
-        interactioncfs[j] * Hinteraction(s1, s2)
+    h = 0.5localcfs[j] * localop(s1) * op("Id", s2) +
+        0.5localcfs[j+1] * op("Id", s1) * localop(s2) +
+        interactioncfs[j] * interactionop(s1, s2)
     push!(list, h)
   end
   return list
 end
 
-# Hamiltoniani
-# ============
-# Hamiltoniani locali
-function Hlocal(s::Index)
+# Operatori locali
+function localop(s::Index)
   if SiteType("S=1/2") ∈ sitetypes(s)
-    h = 0.5op("σz", s)
+    t = 0.5op("σz", s)
   elseif SiteType("Osc") ∈ sitetypes(s)
-    h = op("N", s)
+    t = op("N", s)
+  elseif SiteType("vecS=1/2") ∈ sitetypes(s)
+    t = 0.5im * (op("σz:Id", s) - op("Id:σz", s))
+  elseif SiteType("vecOsc") ∈ sitetypes(s)
+    t = im * (op("N:Id", s) - op("Id:N", s))
   else
     throw(DomainError(s, "SiteType non riconosciuto."))
   end
-  return h
+  return t
 end
 
-# Hamiltoniani di interazione
-function Hinteraction(s1::Index, s2::Index)
+# Operatori di interazione
+function interactionop(s1::Index, s2::Index)
   if SiteType("S=1/2") ∈ sitetypes(s1) && SiteType("S=1/2") ∈ sitetypes(s2)
-    h = -0.5 * (op("σ-", s1) * op("σ+", s2) +
+    t = -0.5 * (op("σ-", s1) * op("σ+", s2) +
                 op("σ+", s1) * op("σ-", s2))
   elseif SiteType("Osc") ∈ sitetypes(s1) && SiteType("S=1/2") ∈ sitetypes(s2)
-    h = op("X", s1) * op("σx", s2)
+    t = op("X", s1) * op("σx", s2)
   elseif SiteType("S=1/2") ∈ sitetypes(s1) && SiteType("Osc") ∈ sitetypes(s2)
-    h = op("σx", s1) * op("X", s2)
+    t = op("σx", s1) * op("X", s2)
   elseif SiteType("Osc") ∈ sitetypes(s1) && SiteType("Osc") ∈ sitetypes(s2)
-    h = (op("a+", s1) * op("a-", s2) +
+    t = (op("a+", s1) * op("a-", s2) +
          op("a-", s1) * op("a+", s2))
-  else
-    throw(DomainError(s, "SiteType non riconosciuto."))
-  end
-  return h
-end
-
-# Lindblad
-# ========
-# Lindblad locali
-function ℓlocal(s::Index)
-  if SiteType("vecS=1/2") ∈ sitetypes(s)
-    ℓ = 0.5im * (op("σz:Id", s) - op("Id:σz", s))
-  elseif SiteType("vecOsc") ∈ sitetypes(s)
-    ℓ = im * (op("N:Id", s) - op("Id:N", s))
-  else
-    throw(DomainError(s, "SiteType non riconosciuto."))
-  end
-  return ℓ
-end
-
-# Lindblad di interazione
-function ℓinteraction(s1::Index, s2::Index)
-  if SiteType("vecS=1/2") ∈ sitetypes(s1) &&
-     SiteType("vecS=1/2") ∈ sitetypes(s2)
-    ℓ = -0.5im * (op("σ-:Id", s1) * op("σ+:Id", s2) +
+  elseif SiteType("vecS=1/2") ∈ sitetypes(s1) &&
+         SiteType("vecS=1/2") ∈ sitetypes(s2)
+    t = -0.5im * (op("σ-:Id", s1) * op("σ+:Id", s2) +
                   op("σ+:Id", s1) * op("σ-:Id", s2) -
                   op("Id:σ-", s1) * op("Id:σ+", s2) -
                   op("Id:σ+", s1) * op("Id:σ-", s2))
   elseif SiteType("vecOsc") ∈ sitetypes(s1) &&
          SiteType("vecS=1/2") ∈ sitetypes(s2)
-    ℓ = im * (op("asum:Id", s1) * op("σx:Id", s2) -
+    t = im * (op("asum:Id", s1) * op("σx:Id", s2) -
               op("Id:asum", s1) * op("Id:σx", s2))
   elseif SiteType("vecS=1/2") ∈ sitetypes(s1) &&
          SiteType("vecOsc") ∈ sitetypes(s2)
-    ℓ = im * (op("σx:Id", s1) * op("asum:Id", s2) -
+    t = im * (op("σx:Id", s1) * op("asum:Id", s2) -
               op("Id:σx", s1) * op("Id:asum", s2))
   else
     throw(DomainError((s1, s2), "SiteType non riconosciuti."))
   end
-  return ℓ
+  return t
 end
