3ee2416a90e2524da34b8aaead0d929163fc86d4
Davide Ferracin <dv.ferracin@gmail.com>
2022-02-14 12:44:50 +0100

    Aggiunto fattore prop. a densità spettrale Lorentz
    Nel confronto tra le densità spettrali del modello unitario e del
    modello con l'equazione di Lindblad, era rimasto fuori il coefficiente
    di interazione tra oscillatore e spin della seconda, che non aveva più
    un ruolo nel modello unitario.
    Questo fattore dovrebbe in realtà apparire come fattore moltiplicativo
    dell'intera densità spettrale, e così ho corretto il programma del
    modello unitario per tenerne conto.

--- unitary_model/unitary_model.jl
+++ unitary_model/unitary_model.jl
@@ -114,8 +114,11 @@ let
     ε = parameters["spin_excitation_energy"]
     # λ = 1
     Ω = parameters["spectral_density_peak"]
-    T = parameters["temperature"]
     γ = parameters["spectral_density_half_width"]
+    κ = parameters["spectral_density_overall_factor"]
+    # La densità spettrale è data da
+    # J(ω) = κγ/π ⋅ 1/(γ² + (ω-Ω)²)
+    T = parameters["temperature"]
     ωc = parameters["frequency_cutoff"]
     osc_dim = parameters["oscillator_space_dimension"]
 
@@ -147,7 +150,7 @@ let
        - n_osc_left+n_spin_sites+1:end -> catena di oscillatori a destra
     =#
     # Calcolo dei coefficienti dalla densità spettrale
-    J(ω) = γ/π * (1 / (γ^2 + (ω-Ω)^2) - 1 / (γ^2 + (ω+Ω)^2))
+    J(ω) = κ * γ/π * (1 / (γ^2 + (ω-Ω)^2) - 1 / (γ^2 + (ω+Ω)^2))
     Jtherm = ω -> thermalisedJ(J, ω, T, (-ωc, ωc))
     Jzero  = ω -> thermalisedJ(J, ω, 0, (0, ωc))
     (Ωₗ, κₗ, ηₗ) = chainmapcoefficients(Jtherm,
