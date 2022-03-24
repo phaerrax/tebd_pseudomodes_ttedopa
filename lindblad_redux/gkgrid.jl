14b21790eb646ea1ea3a25627664ff3ddbc4dce2
Davide Ferracin <dv.ferracin@gmail.com>
2022-03-24 10:19:35 +0100

    Aggiunto script per disegnare grafico griglia
    Ho scritto un progammino che disegna un grafico di livello (un "contour
    plot") della corrente in dipendenza da gamma e kappa assieme.

---
+++ lindblad_redux/gkgrid.jl
@@ -0,0 +1,41 @@
+#!/usr/bin/julia
+
+using Plots
+using DataFrames
+using CSV
+using GR
+
+isdat(name) = name[end-3:end] == ".dat"
+
+function parsegk(name)
+  n = replace(name, "g" => "", ".dat" => "")
+  v = split(n, "k")
+  return parse(Float64, v[1]), parse(Float64, v[2])
+end
+
+function extract(file, nsites)
+  data = DataFrame(CSV.File(file))
+  currents = [data[end, Symbol("current_adjsites$n")] for n ∈ 1:nsites+1]
+  N = length(currents)
+  avg = sum(currents; init=0.0) / N
+  stdev = sqrt(sum((currents .- avg).^2; init=0.0) / (N-1))
+  return [parsegk(basename(file))..., avg, stdev]
+end
+
+let
+  values = mapreduce(permutedims,
+                     vcat,
+                     [extract(dat, 5)
+                      for dat ∈ filter(isdat, readdir(ARGS[1]; join=true))])
+  γ = values[:,1]
+  κ = values[:,2]
+  avg = values[:,3]
+  stdev = values[:,4]
+  output = Dict(:gamma => γ)
+  push!(output, :kappa => κ)
+  push!(output, :Javg => avg)
+  push!(output, :Jstdev => stdev)
+  CSV.write("gkgrid.dat", DataFrame(output))
+  N = isqrt(length(γ))
+  GR.contourf(γ, κ, avg)
+end
