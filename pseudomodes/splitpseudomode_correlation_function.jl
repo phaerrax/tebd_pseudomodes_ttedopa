dfab25b841628b335d2603031c462315a49f3286
Davide Ferracin <dv.ferracin@gmail.com>
2022-05-06 18:45:49 +0200

    Calcolata funzione correlaz. pseudomodo sdoppiato
    Ho scritto un programma che, prendendo l'insieme di parametri per uno
    pseudomodo singolo, crea l'ambiente con lo pseudomodo sdoppiato e ne
    calcola la funzione di correlazione. Nei grafici è effettuato il
    confronto con la funzione attesa, che è la solita, già usata per il
    singolo pseudomodo.

---
+++ pseudomodes/splitpseudomode_correlation_function.jl
@@ -0,0 +1,315 @@
+#!/usr/bin/julia
+
+using ITensors
+using LaTeXStrings
+using ProgressMeter
+using Base.Filesystem
+using DataFrames
+using CSV
+
+# Se lo script viene eseguito su Qtech, devo disabilitare l'output
+# grafico altrimenti il programma si schianta.
+if gethostname() == "qtech.fisica.unimi.it" ||
+  gethostname() == "qtech2.fisica.unimi.it"
+  ENV["GKSwstype"] = "100"
+  @info "Esecuzione su server remoto. Output grafico disattivato."
+else
+  delete!(ENV, "GKSwstype")
+  # Se la chiave "GKSwstype" non esiste non succede niente.
+end
+
+rootdirname = "simulazioni_tesi"
+sourcepath = Base.source_path()
+# Cartella base: determina il percorso assoluto del file in esecuzione, e
+# rimuovi tutto ciò che segue rootdirname.
+ind = findfirst(rootdirname, sourcepath)
+rootpath = sourcepath[begin:ind[end]]
+# `rootpath` è la cartella principale del progetto.
+libpath = joinpath(rootpath, "lib")
+
+include(joinpath(libpath, "utils.jl"))
+include(joinpath(libpath, "plotting.jl"))
+include(joinpath(libpath, "spin_chain_space.jl"))
+include(joinpath(libpath, "harmonic_oscillator_space.jl"))
+include(joinpath(libpath, "operators.jl"))
+
+let  
+  @info "Lettura dei file con i parametri."
+  parameter_lists = load_parameters(ARGS)
+  tot_sim_n = length(parameter_lists)
+
+  # Se il primo argomento da riga di comando è una cartella (che dovrebbe
+  # contenere i file dei parametri), mi sposto subito in tale posizione in modo
+  # che i file di output, come grafici e tabelle, siano salvati insieme ai file
+  # di parametri.
+  prev_dir = pwd()
+  if isdir(ARGS[1])
+    cd(ARGS[1])
+  end
+
+  # Le seguenti liste conterranno i risultati della simulazione per ciascuna
+  # lista di parametri fornita.
+  timesteps_super = []
+  Xcorrelation_super = []
+
+  for (current_sim_n, parameters) in enumerate(parameter_lists)
+    @info "($current_sim_n di $tot_sim_n) Lettura dei parametri dal file."
+
+    κ = parameters["oscillator_spin_interaction_coefficient"]
+    if (haskey(parameters, "oscillator_damping_coefficient_left") &&
+        haskey(parameters, "oscillator_damping_coefficient_right"))
+      γₗ = parameters["oscillator_damping_coefficient_left"]
+      γᵣ = parameters["oscillator_damping_coefficient_right"]
+    elseif haskey(parameters, "oscillator_damping_coefficient")
+      γₗ = parameters["oscillator_damping_coefficient"]
+      γᵣ = γₗ
+    else
+      throw(ErrorException("Oscillator damping coefficient not provided."))
+    end
+    Ω = parameters["oscillator_frequency"]
+    T = parameters["temperature"]
+
+    n(T,ω) = T == 0 ? 0.0 : (ℯ^(ω/T)-1)^(-1)
+
+    # Set new initial oscillator states as empty.
+    parameters["left_oscillator_initial_state"] = "empty"
+
+    # ITensors internal parameters
+    max_err = parameters["MP_compression_error"]
+    max_dim = parameters["MP_maximum_bond_dimension"]
+
+    # Transform the pseudomode into two zero-temperature new modes.
+    ε = parameters["spin_excitation_energy"]
+    # λ = 1
+    κ₁ = κ * sqrt( 1+n(T,Ω) )
+    ω₁ = Ω
+    γ₁ = γₗ
+    #
+    κ₂ = κ * sqrt( n(T,Ω) )
+    ω₂ = -Ω
+    γ₂ = γₗ
+    #
+    κᵣ = κ
+    ωᵣ = Ω
+    #γᵣ = γᵣ
+    #
+    η = 0
+    oscdim = parameters["oscillator_space_dimension"]
+
+    # - intervallo temporale delle simulazioni
+    time_step = parameters["simulation_time_step"]
+    time_step_list = construct_step_list(parameters)
+    skip_steps = parameters["skip_steps"]
+
+    # Costruzione della catena
+    # ========================
+    n_spin_sites = parameters["number_of_spin_sites"]
+    range_spins = 2 .+ (1:n_spin_sites)
+
+    sites = [siteinds("HvOsc", 2; dim=oscdim);
+             siteinds("HvS=1/2", n_spin_sites);
+             siteinds("HvOsc", 1; dim=oscdim)]
+
+    # Definizione degli operatori nell'equazione di Lindblad
+    # ======================================================
+    # Calcolo i coefficienti dell'Hamiltoniano trasformato.
+    ω̃₁ = (ω₁*κ₁^2 + ω₂*κ₂^2 + 2η*κ₁*κ₂) / (κ₁^2 + κ₂^2)
+    ω̃₂ = (ω₂*κ₁^2 + ω₁*κ₂^2 - 2η*κ₁*κ₂) / (κ₁^2 + κ₂^2)
+    κ̃₁ = sqrt(κ₁^2 + κ₂^2)
+    κ̃₂ = ((ω₂-ω₁)*κ₁*κ₂ + η*(κ₁^2 - κ₂^2)) / (κ₁^2 + κ₂^2)
+    # Dato che T = 0 dappertutto, posso semplificare qualcosa.
+    #γ̃₁⁺ = (κ₁^2*γ₁*(n(T,ω₁)+1) + κ₂^2*γ₂*(n(T,ω₂)+1)) / (κ₁^2 + κ₂^2)
+    #γ̃₁⁻ = (κ₁^2*γ₁* n(T,ω₁)    + κ₂^2*γ₂* n(T,ω₂))    / (κ₁^2 + κ₂^2)
+    #γ̃₂⁺ = (κ₂^2*γ₁*(n(T,ω₁)+1) + κ₁^2*γ₂*(n(T,ω₂)+1)) / (κ₁^2 + κ₂^2)
+    #γ̃₂⁻ = (κ₂^2*γ₁* n(T,ω₁)    + κ₁^2*γ₂* n(T,ω₂))    / (κ₁^2 + κ₂^2)
+    #γ̃₁₂⁺ = κ₁*κ₂*( γ₂*(n(T,ω₂)+1) - γ₁*(n(T,ω₁)+1) )  / (κ₁^2 + κ₂^2)
+    #γ̃₁₂⁻ = κ₁*κ₂*( γ₂*n(T,ω₂)     - γ₁*n(T,ω₁) )      / (κ₁^2 + κ₂^2)
+    γ̃₁⁺ = γ₁ # == γ₂
+    γ̃₁⁻ = 0
+    γ̃₂⁺ = γ₂
+    γ̃₂⁻ = 0
+    γ̃₁₂⁺ = 0 # perché γ₁ = γ₂
+    γ̃₁₂⁻ = 0
+
+    localcfs = zeros(Float64, length(sites))
+    localcfs[1] = ω̃₂
+    localcfs[2] = ω̃₁
+    interactioncfs = zeros(Float64, length(sites)-1)
+    interactioncfs[1] = κ̃₂
+
+    @info "($current_sim_n di $tot_sim_n) Costruzione degli operatori di evoluzione temporale."
+    ℓlist = twositeoperators(sites, localcfs, interactioncfs)
+    # Aggiungo agli operatori già creati gli operatori di dissipazione:
+    # rimuovo direttamente quelli nulli.
+    # · per il primo oscillatore a sinistra,
+    ℓlist[1] += γ̃₂⁺ * op("Lindb+", sites[1]) * op("Id", sites[2])
+    #ℓlist[1] += γ̃₂⁻ * op("Lindb-", sites[1]) * op("Id", sites[2])
+    # · per il secondo oscillatore (occhio che non essendo più all'estremo
+    #   della catena questo operatore viene diviso tra ℓ₁,₂ e ℓ₂,₃),
+    ℓlist[1] += 0.5γ̃₁⁺ * op("Id", sites[1]) * op("Lindb+", sites[2])
+    #ℓlist[1] += 0.5γ̃₁⁻ * op("Id", sites[1]) * op("Lindb-", sites[2])
+    ℓlist[2] += 0.5γ̃₁⁺ * op("Lindb+", sites[2]) * op("Id", sites[3])
+    #ℓlist[2] += 0.5γ̃₁⁻ * op("Lindb-", sites[2]) * op("Id", sites[3])
+    # · l'operatore misto su (1) e (2),
+    #ℓlist[1] += γ̃₁₂⁺ * mixedlindbladplus(sites[1], sites[2])
+    #ℓlist[1] += γ̃₁₂⁻ * mixedlindbladminus(sites[1], sites[2])
+    # · infine per l'oscillatore a destra, come al solito,
+    ℓlist[end] += γᵣ * (op("Id", sites[end-1]) *
+                        op("Damping", sites[end]; ω=ωᵣ, T=0))
+    #
+    function links_odd(τ)
+      return [exp(τ * ℓ) for ℓ in ℓlist[1:2:end]]
+    end
+    function links_even(τ)
+      return [exp(τ * ℓ) for ℓ in ℓlist[2:2:end]]
+    end
+
+    # La correlazione
+    # ===============
+    # Detto X l'operatore dell'oscillatore "L1", la funzione di correlazione
+    # da calcolare è
+    # ⟨XₜX₀⟩ = tr(ρ₀XₜX₀) = vec(Xₜ)'vec(X₀ρ₀);
+    # l'evoluzione di X avviene secondo l'equazione di Lindblad aggiunta,
+    # che vettorizzata ha L' al posto di L:
+    # vec(Xₜ) = exp(tL')vec(X₀).
+    # Allora
+    # ⟨XₜX₀⟩ = vec(X₀)'exp(tL')'vec(X₀ρ₀) = vec(X₀)'exp(tL)vec(X₀ρ₀).
+
+    # Simulazione
+    # ===========
+    @info "($current_sim_n di $tot_sim_n) Costruzione dello stato iniziale."
+    # Stato iniziale
+    # --------------
+    # Lo stato iniziale qui è X₀ρ₀ (vedi eq. sopra).
+    # Per calcolare lo stato iniziale dei due oscillatori a sinistra:
+    # 1) la temperatura è sempre 0 quindi lo stato iniziale è
+    #    ρ₀ = |∅⟩ ⟨∅| = |0⟩ ⟨0| ⊗ |0⟩ ⟨0|
+    emptystate = zeros(Float64, oscdim, oscdim)
+    emptystate[1, 1] = 1.0
+    M = (id(oscdim) ⊗ (a⁺(oscdim) + a⁻(oscdim))) * (emptystate ⊗ emptystate)
+    # 2) la vettorizzo sul prodotto delle basi hermitiane dei due siti
+    v = vec(M, [êᵢ ⊗ êⱼ for (êᵢ, êⱼ) ∈ [Base.product(gellmannbasis(oscdim),
+                                                     gellmannbasis(oscdim))...]])
+    # 3) inserisco il vettore in un tensore con gli Index degli oscillatori
+    iv = itensor(v, sites[1], sites[2])
+    # 4) lo decompongo in due pezzi con una SVD
+    f1, f2, _, _ = factorize(iv, sites[1]; which_decomp="svd")
+    # 5) rinomino il Link tra i due fattori come "Link,l=1" anziché
+    #    "Link,fact" che è il Tag assegnato da `factorize`
+    replacetags!(f1, "fact" => "l=1")
+    replacetags!(f2, "fact" => "l=1")
+
+    X₀ρ₀ = chain(MPS([f1, f2]),
+               parse_init_state(sites[range_spins],
+                                parameters["chain_initial_state"]),
+               #MPS([state(sites[end-1], "0")]),
+               MPS([state(sites[end],   "0")]))
+
+    # Osservabili
+    # -----------
+    X₀ = MPS(sites, [i == 2 ? "vecX" : "vecId" for i ∈ eachindex(sites)])
+    correlation(ρ) = κ̃₁^2 * inner(X₀, ρ)
+
+    # Evoluzione temporale
+    # --------------------
+    @info "($current_sim_n di $tot_sim_n) Avvio della simulazione."
+
+    tout, calcXcorrelation = evolve(X₀ρ₀,
+                                    time_step_list,
+                                    parameters["skip_steps"],
+                                    parameters["TS_expansion_order"],
+                                    links_odd,
+                                    links_even,
+                                    parameters["MP_compression_error"],
+                                    parameters["MP_maximum_bond_dimension"];
+                                    fout=[correlation])
+
+    @info "($current_sim_n di $tot_sim_n) Scrittura dei file di output."
+
+    function cᴿ(κ, Ω, γ, T, t)
+      if T != 0
+        κ^2 * (coth(0.5*Ω/T)*cos(Ω*t) - im*sin(Ω*t)) * ℯ^(-0.5γ*t)
+      else
+        κ^2 * ℯ^(-im*Ω*t - 0.5γ*t)
+      end
+    end
+    expXcorrelation = [cᴿ(κ,Ω,γₗ,T, t) for t ∈ tout]
+    Xcorrelation = hcat(calcXcorrelation, expXcorrelation)
+
+    # Creo una tabella con i dati rilevanti da scrivere nel file di output
+    dict = Dict(:time => tout)
+    push!(dict, :correlation_calc_re => real.(calcXcorrelation))
+    push!(dict, :correlation_calc_im => imag.(calcXcorrelation))
+    push!(dict, :correlation_exp_re  => real.(expXcorrelation))
+    push!(dict, :correlation_exp_im  => imag.(expXcorrelation))
+    table = DataFrame(dict)
+    filename = replace(parameters["filename"], ".json" => ".dat")
+    # Scrive la tabella su un file che ha la stessa estensione del file dei
+    # parametri, con estensione modificata.
+    CSV.write(filename, table)
+
+    # Salvo i risultati nei grandi contenitori
+    push!(timesteps_super, tout)
+    push!(Xcorrelation_super, Xcorrelation)
+  end
+
+  @info "Creazione dei grafici."
+  # Grafici
+  # =======
+  # Come funziona: creo un grafico per ogni tipo di osservabile misurata. In
+  # ogni grafico, metto nel titolo tutti i parametri usati, evidenziando con
+  # la grandezza del font o con il colore quelli che cambiano da una
+  # simulazione all'altra.
+  #
+  plotsize = (600, 400)
+
+  distinct_p, repeated_p = categorise_parameters(parameter_lists)
+
+  # Grafico della funzione di correlazione
+  # --------------------------------------
+  data = [[real.(Xcorrelation[:,1])  real.(Xcorrelation[:,2])]
+          for Xcorrelation ∈ Xcorrelation_super]
+  plt = groupplot(timesteps_super,
+                  data,
+                  parameter_lists;
+                  labels=["calculated" "expected"],
+                  linestyles=[:solid :dash],
+                  commonxlabel=L"t",
+                  commonylabel=L"\mathrm{Re}\,(\langle X(t)X(0)\rangle)",
+                  plottitle="Funzione di correlazione (parte reale)",
+                  plotsize=plotsize)
+
+  savefig(plt, "Xcorrelation_re.png")
+
+  data = [[imag.(Xcorrelation[:,1])  imag.(Xcorrelation[:,2])]
+          for Xcorrelation ∈ Xcorrelation_super]
+  plt = groupplot(timesteps_super,
+                  data,
+                  parameter_lists;
+                  labels=["calculated" "expected"],
+                  linestyles=[:solid :dash],
+                  commonxlabel=L"t",
+                  commonylabel=L"\mathrm{Im}\,(\langle X(t)X(0)\rangle)",
+                  plottitle="Funzione di correlazione (parte immaginaria)",
+                  plotsize=plotsize)
+
+  savefig(plt, "Xcorrelation_im.png")
+
+  data = [[real.(Xcorrelation[:,1]) .- real.(Xcorrelation[:,2]);;
+           imag.(Xcorrelation[:,1]) .- imag.(Xcorrelation[:,2])]
+          for Xcorrelation ∈ Xcorrelation_super]
+  plt = groupplot(timesteps_super,
+                  data,
+                  parameter_lists;
+                  labels=["real part" "imag part"],
+                  linestyles=[:solid :dash],
+                  commonxlabel=L"t",
+                  commonylabel=L"\langle X(t)X(0)\rangle_{calc}-\langle X(t)X(0)\rangle_{exp}",
+                  plottitle="Diff. funzioni di correlazione",
+                  plotsize=plotsize)
+
+  savefig(plt, "Xcorrelation_diff.png")
+
+  cd(prev_dir) # Il lavoro è completato: ritorna alla cartella iniziale.
+  return
+end
