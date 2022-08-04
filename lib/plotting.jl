ebc0d8faafb0a28781391be986cdf5d852758b80
Davide Ferracin <dv.ferracin@gmail.com>
2022-08-04 16:57:03 +0200

    Semplificati titoli dei grafici con solo nome file
    In seguito alle ultime modifiche degli script, il numero di parametri
    comincia a diventare troppo grande, quindi era il caso di individuare un
    altro modo di distinguere i sottografici all'interno di un gruppo. Il
    modo più immediato è indicare il nome del file dei parametri, quindi ho
    aggiunto la possibilità di ignorare tutta quella storia dei parametri
    distinti e comuni che c'è sempre stata, e usare solo il nome del file
    come indicatore.

--- lib/plotting.jl
+++ lib/plotting.jl
@@ -62,7 +62,7 @@ function subplot_title(values_dict, keys)
 end
 
 """
-    shared_title_fake_plot(subject::String, parameters)
+    shared_title_fake_plot(subject::String, parameters; filenameastitle = false)
     
 Create a fake plot containing only a string, which acts as the title of a
 group of other subplots.
@@ -71,36 +71,43 @@ Since Plots.jl does not yet provide a mean to assign a title to a plot group,
 we make do with this function.
 The title will contain the parameters which are common to all subplots.
 """
-function shared_title_fake_plot(subject::String, parameters)
-  # Siccome l titolone contiene i parametri comuni a tutte le simulazioni,
+function shared_title_fake_plot(subject::String,
+                                parameters;
+                                filenameastitle = false)
+  # Siccome il titolone contiene i parametri comuni a tutte le simulazioni,
   # posso prendere senza problemi uno degli elementi di parameter_lists
   # qualunque) e lo uso come titolo del gruppo di grafici.
-  #
-  # Inserire in questo array i parametri che non si vuole che appaiano nel
-  # titolo:
-  hidden_parameters = ["simulation_end_time", "skip_steps", "filename"]
-  _, repeated_parameters = categorise_parameters(parameters)
-  #
-  rootdirname = "simulazioni_tesi"
-  sourcepath = Base.source_path()
-  ind = findfirst(rootdirname, sourcepath)
-  rootpath = sourcepath[begin:ind[end]]
-  libpath = joinpath(rootpath, "lib")
-  f = open(joinpath(libpath, "short_names_dictionary.json"), "r")
-  # Carico il dizionario dei "nomi brevi" per i parametri.
-  short_name = JSON.parse(read(f, String))
-  close(f)
-  #
-  shared_title = join([short_name[k] * "=" * string(parameters[begin][k])
-                       for k in setdiff(repeated_parameters, hidden_parameters)],
-                      ", ")
+  
+  if filenameastitle
+    bigtitle = subject
+  else
+    # Inserire in questo array i parametri che non si vuole che appaiano nel
+    # titolo:
+    hidden_parameters = ["simulation_end_time", "skip_steps", "filename"]
+    _, repeated_parameters = categorise_parameters(parameters)
+    #
+    rootdirname = "simulazioni_tesi"
+    sourcepath = Base.source_path()
+    ind = findfirst(rootdirname, sourcepath)
+    rootpath = sourcepath[begin:ind[end]]
+    libpath = joinpath(rootpath, "lib")
+    f = open(joinpath(libpath, "short_names_dictionary.json"), "r")
+    # Carico il dizionario dei "nomi brevi" per i parametri.
+    short_name = JSON.parse(read(f, String))
+    close(f)
+    #
+    shared_title = join([short_name[k] * "=" * string(parameters[begin][k])
+                         for k in setdiff(repeated_parameters, hidden_parameters)],
+                        ", ")
+    bigtitle = subject * "\n" * shared_title
+  end
   y = ones(3) # Dati falsi per far apparire un grafico
-  title_fake_plot = Plots.scatter(y, marker=0, markeralpha=0, ticks=nothing, annotations=(2, y[2], Plots.text(subject * "\n" * shared_title)), axis=false, grid=false, leg=false, bottom_margin=2cm, size=(200,100))
+  title_fake_plot = Plots.scatter(y, marker=0, markeralpha=0, ticks=nothing, annotations=(2, y[2], Plots.text(bigtitle)), axis=false, grid=false, leg=false, bottom_margin=2cm, size=(200,100))
   return title_fake_plot
 end
 
 """
-    groupplot(x_super, y_super, parameter_super; maxyrange = nothing, rescale = true, labels, linestyles, commonxlabel, commonylabel, plottitle, plotsize)
+    groupplot(x_super, y_super, parameter_super; maxyrange = nothing, rescale = true, filenameastitle = false, labels, linestyles, commonxlabel, commonylabel, plottitle, plotsize)
 
 Create an image which groups the plots of each ``(X,Y) ∈ x_super × y_super``;
 on top of the plots, a title is created which lists the common parameters among
@@ -122,7 +129,8 @@ above each subplot.
 - `commonylabel` → y-axis label (common to all subplots).
 - `maxyrange` → y-axis range at which the subplots may be clipped
 - `rescale` → if `true`, all subplots are rescaled so that they have the same
-  y-axis.
+- `filenameastitle` → if `true`, the title of each subplot will be given by
+  the filename of the JSON file describing the parameters of the simulation
 - `plottitle` → title of the plot group.
 - `plotsize` → a Pair denoting the size of the individual subplots.
 """
@@ -131,6 +139,7 @@ function groupplot(x_super,
     parameter_super;
     maxyrange = nothing,
     rescale = true,
+    filenameastitle = true,
     labels,
     linestyles,
     commonxlabel,
@@ -195,7 +204,14 @@ function groupplot(x_super,
   end
 
   # Smisto i parametri in ripetuti e non, per creare i titoli dei grafici.
-  distinct_parameters, _ = categorise_parameters(parameter_super)
+  if filenameastitle
+    subplottitles = [p["filename"] for p ∈ parameter_super]
+  else
+    distinct_parameters, _ = categorise_parameters(parameter_super)
+    subplottitles = [subplot_title(p, distinct_parameters)
+                     for p ∈ parameter_super]
+  end
+
   # Calcolo la grandezza totale dell'immagine a partire da quella dei grafici.
   figuresize = (2, Int(ceil(length(x_super)/2))+0.5) .* plotsize
 
@@ -208,16 +224,17 @@ function groupplot(x_super,
                          legend=:outerright,
                          xlabel=commonxlabel,
                          ylabel=commonylabel,
-                         title=subplot_title(p, distinct_parameters),
+                         title=sptitle,
                          left_margin=5mm,
                          bottom_margin=5mm,
                          size=figuresize)
-              for (X, Y, lab, lst, ylimits, p) ∈ zip(x_super,
-                                                     y_super,
-                                                     labels,
-                                                     linestyles,
-                                                     ylimits_super,
-                                                     parameter_super)]
+              for (X, Y, lab, lst, ylimits, sptitle, p) ∈ zip(x_super,
+                                                              y_super,
+                                                              labels,
+                                                              linestyles,
+                                                              ylimits_super,
+                                                              subplottitles,
+                                                              parameter_super)]
   # I grafici saranno disposti in una griglia con due colonne; se ho un numero
   # dispari di grafici, ne creo uno vuoto in modo da riempire il buco che
   # si crea (altrimenti mi becco un errore).
@@ -233,7 +250,9 @@ function groupplot(x_super,
     push!(subplots, fakeplot)
   end
   # Creo il grafico che raggruppa tutto, insieme al titolo principale.
-  group = Plots.plot(shared_title_fake_plot(plottitle, parameter_super),
+  group = Plots.plot(shared_title_fake_plot(plottitle,
+                                            parameter_super;
+                                            filenameastitle),
                      Plots.plot(subplots..., layout=(length(subplots)÷2, 2)),
                      # Usa ÷ e non /, in modo da ottenere un Int!
                      layout=Plots.grid(2, 1, heights=[0.1, 0.9]))
@@ -241,7 +260,7 @@ function groupplot(x_super,
 end
 
 """
-    unifiedplot(x_super, y_super, parameter_super; linestyle, xlabel, ylabel, plottitle, plotsize)
+    unifiedplot(x_super, y_super, parameter_super; filenameastitle = false, linestyle, xlabel, ylabel, plottitle, plotsize)
 
 Create a plot where all data series (X,Y) in `x_super` × `y_super` are drawn
 together. A title is shown on top, which lists the common parameters among the
@@ -253,22 +272,30 @@ in the label of each series.
 - `y_super` → list of lists, corresponding to the relative element in `x_super`.
 - `parameter_lists` → list of dictionaries, containing the parameters
   associated to each element in `x_super`.
+- `filenameastitle` → if `true`, the label of each subplot will be given by
+  the filename of the JSON file describing the parameters of the simulation
 - `linestyle` → line styles of the plots
 - `xlabel` → x-axis label.
 - `ylabel` → y-axis label.
 - `plottitle` → title of the plot.
 - `plotsize` → a Pair representing the plot size (in pixels).
 """
-function unifiedplot(x_super, y_super, parameter_super; linestyle, xlabel, ylabel, plottitle, plotsize)
+function unifiedplot(x_super, y_super, parameter_super; filenameastitle = false, linestyle, xlabel, ylabel, plottitle, plotsize)
   # Smisto i parametri in ripetuti e non, per creare le etichette dei grafici.
-  distinct_parameters, _ = categorise_parameters(parameter_super)
+  if filenameastitle
+    labels = [p["filename"] for p ∈ parameter_super]
+  else
+    distinct_parameters, _ = categorise_parameters(parameter_super)
+    labels = [subplot_title(p, distinct_parameters)
+                     for p ∈ parameter_super]
+  end
   # Imposto la dimensione della figura (con un po' di spazio per il titolo).
   figuresize = (1, 1.25) .* plotsize
   plt = Plots.plot()
-  for (X, Y, p) in zip(x_super, y_super, parameter_super)
+  for (X, Y, lb, p) in zip(x_super, y_super, labels, parameter_super)
     plt = Plots.plot!(X,
                       Y,
-                      label=subplot_title(p, distinct_parameters),
+                      label=lb,
                       linestyle=linestyle,
                       legend=:outerbottom,
                       xlabel=xlabel,
@@ -280,22 +307,18 @@ function unifiedplot(x_super, y_super, parameter_super; linestyle, xlabel, ylabe
 end
 
 """
-    unifiedlogplot(x_super, y_super, parameter_super; linestyle, xlabel, ylabel, plottitle, plotsize)
+    unifiedlogplot(x_super, y_super, parameter_super; <keyword arguments>)
 
 Create a plot where all data series (X,Y) in `x_super` × `y_super` are drawn
 together, with the y-axis in logarithmic scale.
-See `unifiedplot` for more details on the arguments.
+See `unifiedplot` for more details on the keyword arguments.
 
 Note that no check is performed whether the arguments of the logarithm are
 positive.
 """
-function unifiedlogplot(x_super, y_super, parameter_super; linestyle, xlabel, ylabel, plottitle, plotsize)
+function unifiedlogplot(x_super, y_super, parameter_super; kwargs...)
   return unifiedplot(x_super,
                      [log.(Y) for Y ∈ y_super],
                      parameter_super;
-                     linestyle=linestyle,
-                     xlabel=xlabel,
-                     ylabel=ylabel,
-                     plottitle=plottitle,
-                     plotsize=plotsize)
+                     kwargs...)
 end
